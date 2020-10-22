f0ee7bdf0ca3592a091ffebc75edb936d0209ed5
yamori-masato <yamorimasato@gmail.com>
2020-10-23 01:50:52 +0900

    javascript/channelsを自動読み込みから除外

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
 
-  root to: 'users#index'
+
+  root to: 'users#show'
+  # get '/rooms/show/:id', to: 'room#show'
+  resources :rooms
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
