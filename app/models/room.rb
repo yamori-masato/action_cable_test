fa90f7380046b6c4afcdc96bd5b50d67c83058a1
yamori-masato <yamorimasato@gmail.com>
2020-10-23 22:54:12 +0900

    モデル間の関連付けを修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-    has_many :messages, dependent: :destroy
+    has_many :messages, foreign_key: :room_id, dependent: :destroy
     has_many :user_rooms
     has_many :users, through: :user_rooms
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :messages, foreign_key: :user_id, dependent: :destroy
     has_many :user_rooms
     has_many :rooms, through: :user_rooms
 end
