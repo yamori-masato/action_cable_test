7421141144d0cd3edf095f65eccf9afaaec0fb8d
yamori-masato <yamorimasato@gmail.com>
2020-10-23 22:54:40 +0900

    seed値を設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+2.times do |i|
+    User.create!(
+        name: "user#{i+1}",
+        password: "pass",
+        password_confirmation: "pass",
+    )
+end
